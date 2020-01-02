1159e043865fff9b38b437d4721c85613e18cb3c
She-Codes <n.y.shaw@gmail.com>
2020-01-01 19:36:21 -0500

    removed unused methods in Pager

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -1,18 +1,6 @@
 import React from 'react';
 
 const Pager = props => {
-  // ex url: "https://rickandmortyapi.com/api/character/?page=2"
-  const getPageNum = url => {
-    const numberString = url.split("page=")[1];
-    return parseInt(numberString, 10);
-  };
-
-  const getCurrentPageNum = () => {
-    return props.nextPageUrl
-      ? getPageNum(props.nextPageUrl) - 1
-      : getPageNum(props.prevPageUrl) + 1;
-  };
-
   return (
     props.nextPageUrl &&
     <div>
